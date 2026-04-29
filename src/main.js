a0a80f633b17fd29d988d11f47e347831e48b309
Harsh <yourdevharsh@gmail.com>
2026-04-29 18:03:57 +0530

    Completed level 3 too, needs CSS, bug fixes and refinements.

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ let totalExpense = 0;
 
 if (localStorage.getItem("salary")) {
   salaryAmtEl.innerText = localStorage.getItem("salary");
+  salaryAmtEl.setAttribute("data-baseValue", `${localStorage.getItem("salary")}`);
   toggleExpInputs(false);
 }
 
@@ -31,7 +32,7 @@ if (localStorage.getItem("expenses")) {
     const liEl = document.createElement("li");
     liEl.className = "expItem";
 
-    liEl.innerHTML = `<span>${expense[0]}</span> <span>${expense[1]}</span> <button class="deleteItemBtn">Delete</button>`;
+    liEl.innerHTML = `<span>${expense[0]}</span> <span class="currencySign">₹</span> <span class="convertible" data-baseValue="${expense[1]}">${expense[1]}</span> <button class="deleteItemBtn">Delete</button>`;
     storedExpense += expense[1];
 
     frag.appendChild(liEl);
@@ -41,9 +42,13 @@ if (localStorage.getItem("expenses")) {
   expListEl.appendChild(frag);
 
   remainBal.innerText = `${Number(salaryAmtEl.innerText) - totalExpense}`;
+  remainBal.setAttribute("data-baseValue" , `${Number(salaryAmtEl.innerText) - totalExpense}`);
 } else {
   remainBal.innerText = `${Number(salaryAmtEl.innerText)}`;
+  remainBal.setAttribute("data-baseValue", `${Number(salaryAmtEl.innerText)}`);
 }
+
+updateColor();
 
 // LISTEN & UPDATE FOR SALARY INPUT
 salaryInpEl.addEventListener("input", () => {
@@ -51,13 +56,18 @@ salaryInpEl.addEventListener("input", () => {
 
   if (salaryValue > 0) {
     salaryAmtEl.innerText = `${salaryValue}`;
+    salaryAmtEl.setAttribute("data-baseValue", `${salaryValue}`);
     saveSalaryInStorage(salaryValue);
     toggleExpInputs(false);
   } else {
     salaryAmtEl.innerText = `0`;
+    salaryAmtEl.setAttribute("data-baseValue", "0");
     deleteSalaryFromStorage();
     toggleExpInputs(true);
   }
+
+  remainBal.innerText = Number(salaryAmtEl.innerText) - totalExpense;
+  remainBal.setAttribute("data-baseValue", `${Number(salaryAmtEl.innerText) - totalExpense}`);
 });
 
 function saveSalaryInStorage(value) {
@@ -102,11 +112,14 @@ addExpBtnEl.addEventListener("click", (e) => {
 
   totalExpense += Number(expAmtValue);
   remainBal.innerText = Number(salaryAmtEl.innerText) - totalExpense;
+  remainBal.setAttribute("data-baseValue", `${Number(salaryAmtEl.innerText) - totalExpense}`);
+
+  updateColor();
 
   const liEl = document.createElement("li");
   liEl.className = "expItem";
 
-  liEl.innerHTML = `<span>${expNameValue}</span> <span>${expAmtValue}</span> <button class="deleteItemBtn">Delete</button>`;
+  liEl.innerHTML = `<span>${expNameValue}</span> <span class="currencySign">₹</span> <span class="convertible" data-baseValue="${expAmtValue}">${expAmtValue}</span> <button class="deleteItemBtn">Delete</button>`;
 
   expListEl.appendChild(liEl);
 
@@ -125,12 +138,21 @@ addExpBtnEl.addEventListener("click", (e) => {
   validateExpenseInputs();
 });
 
+function updateColor() {
+  if (Number(remainBal.innerText) < 0.1*Number(salaryAmtEl.innerText)) {
+    remainBal.style.color = "red";
+    alert("Warning: Budget is too low!");
+  } else {
+    remainBal.style.color = "black";
+  }
+}
+
 expListEl.addEventListener("click", (e) => {
   if (e.target.classList.contains("deleteItemBtn")) {
     const liEl = e.target.closest("li");
 
     const delExpName = liEl.children[0].innerText;
-    const delExpAmt = Number(liEl.children[1].innerText);
+    const delExpAmt = Number(liEl.children[2].innerText);
 
     expenses = JSON.parse(localStorage.getItem("expenses"));
 
@@ -144,6 +166,9 @@ expListEl.addEventListener("click", (e) => {
     totalExpense -= delExpAmt;
 
     remainBal.innerText = `${Number(salaryAmtEl.innerText) - totalExpense}`;
+    remainBal.setAttribute("data-baseValue", `${Number(salaryAmtEl.innerText) - totalExpense}`);
+
+    deleteFromChart(pieChart, delExpName, delExpAmt);
 
     if (expenses.length == 0) {
       localStorage.removeItem("expenses");
@@ -152,12 +177,14 @@ expListEl.addEventListener("click", (e) => {
     }
 
     expListEl.removeChild(liEl);
+
+    updateColor();
   }
 });
 
-const chartCanvas = document.getElementById("chart");
+/* PIE CHART */
 
-console.log(expenses);
+const chartCanvas = document.getElementById("chart");
 
 const pieChart = new Chart(chartCanvas, {
   type: "pie",
@@ -183,8 +210,103 @@ const pieChart = new Chart(chartCanvas, {
 });
 
 function addDataToChart(chart, label, newData) {
+  chart.data.labels.shift();
+  chart.data.labels.unshift("Remaining");
+  chart.data.datasets[0].data.shift();
+  chart.data.datasets[0].data.unshift(Number(remainBal.innerText));
   chart.data.labels.push(label);
   chart.data.datasets[0].data.push(newData);
 
   chart.update();
 }
+
+function deleteFromChart(chart, label, value) {
+  let labels = [...chart.data.labels];
+  let data = [...chart.data.datasets[0].data];
+  labels[0] = "Remaining";
+  data[0] = Number(remainBal.innerText);
+  for (let i = 1; i < labels.length; i++) {
+    if (label == labels[i] && value == data[i]) {
+      labels.splice(i, 1);
+      data.splice(i, 1);
+    }
+  }
+
+  chart.data.labels = labels;
+  chart.data.datasets[0].data = data;
+
+  chart.update();
+}
+
+/* GENERATE PDF REPORT */
+
+const genPdfBtn = document.getElementById("genPdfBtn");
+
+genPdfBtn.addEventListener("click", () => {
+  if (expenses.length == 0) {
+    return;
+  }
+
+  const report = new window.jspdf.jsPDF();
+
+  const startX = 10;
+  const startY = 20;
+  const cellWidth = 40;
+  const cellHeight = 10;
+
+  const tableData = expenses.map(expense => [expense[0], String(expense[1])]);
+  tableData.push(["Remaining", `${remainBal.innerText}`]);
+  const rows = tableData;
+
+  rows.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      const x = startX + colIndex * cellWidth;
+      const y = startY + rowIndex * cellHeight;
+
+      report.rect(x, y, cellWidth, cellHeight);
+
+      report.text(cell, x + 2, y + 7);
+    });
+  });
+
+  report.save("manual-table.pdf");
+});
+
+/* RATE CONVERSION */
+
+async function getRate(to, from) {
+  try {
+    const response = await fetch(`https://api.frankfurter.dev/v2/rate/${to}/${from}`);
+    const data = await response.json();
+    const rate = data["rate"];
+    return rate;
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error);
+  }
+}
+
+const currncySelectEl = document.getElementById('currencySelect');
+
+const signs = document.querySelectorAll(".currencySign");
+const symbols = { "INR": "₹", "USD": "$", "EUR": "€" };
+
+currncySelectEl.addEventListener("input", async (e) => {
+  const newCurrency = e.target.value;
+  
+  let rate = 1;
+
+  if (newCurrency != "INR") {
+    rate = await getRate("INR", newCurrency);
+  }
+
+  const convertibles =  document.querySelectorAll(".convertible");
+  
+  convertibles.forEach((conv) => {
+    const baseValue = Number(conv.getAttribute("data-baseValue"));
+    const converted = baseValue * rate;
+
+    conv.innerText = converted.toFixed(2);
+  });
+
+  signs.forEach(s => s.innerText = symbols[newCurrency]);
+});
